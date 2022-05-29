5fdaadfd3b2623f92fa57c336a0cb024a42036eb
Joe <Joe_Zou@epam.com>
2022-05-29 21:42:31 +0800

    build(scss): introduce scss module and mini css extract plugin

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -1,6 +1,9 @@
 const path = require("path");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 const Dotenv = require("dotenv-webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const isDevelopment = process.env.NODE_ENV === "development";
 
 module.exports = {
   entry: "./src/index.tsx",
@@ -9,7 +12,7 @@ module.exports = {
     filename: "index_bundle.js",
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: [".tsx", ".ts", ".js", "scss"],
   },
   module: {
     rules: [
@@ -20,9 +23,49 @@ module.exports = {
           loader: "babel-loader",
         },
       },
+      {
+        test: /\.module\.s(a|c)ss$/,
+        use: [
+          isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              sourceMap: isDevelopment,
+            },
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: isDevelopment,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        exclude: /\.module\.s(a|c)ss$/,
+        use: [
+          // 在开发过程中回退到 style-loader
+          isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: isDevelopment,
+            },
+          },
+        ],
+      },
     ],
   },
   plugins: [
+    new MiniCssExtractPlugin({
+      // 与 webpackOptions.output 中的选项相似
+      // 所有的选项都是可选的
+      filename: isDevelopment ? "[name].css" : "[name].[hash].css",
+      chunkFilename: isDevelopment ? "[id].css" : "[id].[hash].js",
+    }),
     new HTMLWebpackPlugin({
       template: "./index.html",
     }),
